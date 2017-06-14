e5e1989267942d3adcf99418f97a8b8762f69620
void7 <jonathansuperzero@gmail.com>
2017-06-14 13:08:27 -0400

    Update Debt.java

--- Debt.java
+++ Debt.java
@@ -1,61 +1,60 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+   import java.util.*;
+   import java.lang.*;
+   import java.io.*;
 
-public class Debt extends Category
-{
- double rate = 0.05;
- CashFlow cashflow = new CashFlow();
- double debt;
- 
- //constructor:
- public Debt()
- {
-  super("debt");
+    public class Debt extends Category
+   {
+      double rate = 0.05;
+      CashFlow cashflow = new CashFlow();
+      double debt;
    
- }
- 
- //accessor:
- public void setRate(double rate)
- {
-  this.rate = rate;
- }
- 
- //methods:
- public double interest(double[] cash,int n)
- {
-  if(cash[n]>= 0)
-   return 0;
-  else
-   return (cash[n]+interest(cash,n-1))*rate;
- }
- 
- public void updateDebt()
- {
-	 
-	 numCategory = cashflow.numCashflow;
-	 
-	 for(int i = numCategory-1;i>=0;i--)
-	 {
-		 time[i] = cashflow.time[i];
-		 amount[i] = Math.abs(Math.round((interest(cashflow.amount,i))));
-	 }
-	   
- }
- 
- public double getDebt(Time start,Time end)
- {
- 	Time t = start.copy();
- 	debt = 0;
- 	while((t.compare(end)<= 0))
-	{
-		debt += amountMonth(t);
-		t.addMonth();
-	}
-	return debt;
- }
- 
- 
- 
-}
-  
+   //constructor:
+       public Debt()
+      {
+         super("debt");
+      
+      }
+   
+   //accessor:
+       public void setRate(double rate)
+      {
+         this.rate = rate;
+      }
+   
+   //methods:
+       public double interest(double[] cash,int n)
+      {
+         if(cash[n]>= 0)
+            return 0;
+         else
+            return (cash[n]+interest(cash,n-1))*rate;
+      }
+   
+       public void updateDebt()
+      {
+      
+         numCategory = cashflow.numCashflow;
+      
+         for(int i = numCategory-1;i>=0;i--)
+         {
+            time[i] = cashflow.time[i];
+            amount[i] = Math.abs(Math.round((interest(cashflow.amount,i))));
+         }
+      
+      }
+   
+       public double getDebt(Time start,Time end)
+      {
+         Time t = start.copy();
+         debt = 0;
+         while((t.compare(end)<= 0))
+         {
+            debt += amountMonth(t);
+            t.addMonth();
+         }
+         return debt;
+      }
+   
+   
+   
+   }
